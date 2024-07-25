efd42a43ed6c52a086e9d6f7e783a1b0274f644b
southAndy <zatchbell.1206@gmail.com>
2024-07-25 21:43:30 +0800

    chore: modify deploy's setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,14 @@ const config = {
   // favicon: "img/favicon.ico", 
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/note/",
+  url: "https://southandy.github.io",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "southAndy", // Usually your GitHub org/user name.
   projectName: "note", // Usually your repo name.
+  deploymentBranch:'gh-pages',
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
