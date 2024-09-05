d54c3686a8b7632e75a6efa8de82a30fcc4ca3ee
southAndy <zatchbell.1206@gmail.com>
2024-09-05 16:15:11 +0800

    chore(project):remove edit link's settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,8 +45,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          
         },
         blog: {
           showReadingTime: true,
@@ -139,6 +138,7 @@ const config = {
         theme: prismThemes.dracula, // lightTheme's setting
         darkTheme: prismThemes.dracula, // darkTheme's setting
       },
+      editCurrentVersion:false
     }),
 };
 
