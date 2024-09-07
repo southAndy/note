8828c1d6fc77cfae717a88b428474e6293557086
southAndy <zatchbell.1206@gmail.com>
2024-09-07 20:51:38 +0800

    style(home):remove default header's content

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -13,9 +13,10 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
+          {/* {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))}
+          ))} */}
+          
         </div>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,9 @@ import styles from "./index.module.css";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header >
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        
       </div>
     </header>
   );
